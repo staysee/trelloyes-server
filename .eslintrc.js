870f4db332762ac4e6fa9bb98b37ac4e6baeb39d
Stacey Sugiono <ssugiono@vacco.com>
2019-12-10 14:53:13 -0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "supertest": true,
+        "expect": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Trelloyes Server
