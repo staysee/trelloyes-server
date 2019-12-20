e04219a44bce64476c018b8779b4c9686220c5ec
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-12-20 11:52:48 -0800

    modularizing with router

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const uuid = require('uuid/v4')
 const { NODE_ENV } = require('./config')
+const cardRouter = require('./card/card-router')
+const listRouter = require('./list/list-router')
 
 const app = express()
 
@@ -11,13 +14,31 @@ const morganOption = (NODE_ENV === 'production')
     ? 'tiny' 
     : 'common'
 
+
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`)
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+})
+
+app.use(cardRouter)
+app.use(listRouter)
+
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
+
 
 app.use(function errorHandler(error, req, res, next) {
     let response
